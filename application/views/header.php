de16909730c7d869a8d30a27f42335e6d9eba9f9
Arielle Davenport <arielle.davenport21@gmail.com>
2016-07-18 19:15:50 -0500

    fix css problem

--- application/views/header.php
+++ application/views/header.php
@@ -20,7 +20,7 @@
 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-2.2.3.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.interactive_bg.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.photoset-grid.js"); ?>"></script>
 
 <script>
