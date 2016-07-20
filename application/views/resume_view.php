1e8d85243be6a4c82685a93fe5021ecaa5c9115d
Arielle Davenport <arielle.davenport21@gmail.com>
2016-07-19 22:34:04 -0500

    updated length

--- application/views/resume_view.php
+++ application/views/resume_view.php
@@ -1,8 +1,8 @@
 <?php include_once('header.php'); ?>
 	<div class="text-center">
-	<object data="<?php echo base_url("assets/images/resume.pdf"); ?>" type="application/pdf" width="100%" height="500" id="res">
+	<object data="<?php echo base_url("assets/images/resume.pdf"); ?>" type="application/pdf" width="100%" height="800px" id="res">
 		<!-- support for older browsers -->
-		<embed src="<?php echo base_url("assets/images/resume.pdf"); ?>" type="application/pdf" width="100%" height="500" id="res"/>
+		<embed src="<?php echo base_url("assets/images/resume.pdf"); ?>" type="application/pdf" width="100%" height="800px" id="res"/>
 		<!-- for those without native support, no pdf plugin or javascript  -->
 		<p> It appears you do not have PDF support in this web browser</p>
 	</object>
