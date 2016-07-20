d1f10f0c9a6e278f27116dd3778356b01e4d1fe4
Arielle Davenport <arielle.davenport21@gmail.com>
2016-07-20 09:47:14 -0500

    final
    final changes with pdf sizing, url.

--- application/views/resume_view.php
+++ application/views/resume_view.php
@@ -1,14 +1,14 @@
 <?php include_once('header.php'); ?>
-	<div class="text-center">
-	<object data="<?php echo base_url("assets/images/resume.pdf"); ?>" type="application/pdf" width="100%" height="800px" id="res">
-		<!-- support for older browsers -->
-		<embed src="<?php echo base_url("assets/images/resume.pdf"); ?>" type="application/pdf" width="100%" height="800px" id="res"/>
-		<!-- for those without native support, no pdf plugin or javascript  -->
-		<p> It appears you do not have PDF support in this web browser</p>
-	</object>
+	<div id="editor">
+		<a href="<?php echo base_url("assets/images/resume.pdf"); ?>" download="arielle_davenport_resume" type="button" class="btn btn-primary" data-dismiss="modal">Download Resume</a>
+	</div>
+	<div class="text-center pdfdoc">
+	<iframe src='<?php echo base_url("assets/images/resume.pdf"); ?>' width='1100' height='850' frameborder='1' scrolling="auto" >
+	</iframe>
 	</div>
 
 <?php include_once('footer.php'); ?>
 <style>
 #res{position:relative; z-index:100;}
+.btn-primary{position:relative; z-index:100;}
 </style>
